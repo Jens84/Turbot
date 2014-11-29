522d0a2edc9b5787053b6ea137199b5a9a225533
José Esteves <josepmesteves@hotmail.com>
2014-11-29 18:34:49 +0100

    flake8 corrections

--- turbot/learn/pickleHandler.py
+++ turbot/learn/pickleHandler.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-#import pickle
+# import pickle
 import cPickle as pickle
 import os
 import dialog
@@ -10,9 +10,11 @@ import markov
 
 def save_object(obj, filename):
 
-    path1 = "/Users/joseesteves/Documents/Erasmus/DTU/Data Mining/Git/Repository/turbot/learn/"
+    path1 = "/Users/joseesteves/Documents/Erasmus/DTU/Data Mining/Git/"
+    "Repository/turbot/learn/"
     path2 = "/home/beljul/DTU/Data mining using Python/Project/turbot/learn/"
-    path3 = "/home/jens/Documents/DTU/Data Mining Using Python/Project/turbot/learn/"
+    path3 = "/home/jens/Documents/DTU/Data Mining Using Python/Project/turbot/"
+    "learn/"
 
     if os.path.exists(path1):
         filename = path1 + filename
@@ -21,17 +23,20 @@ def save_object(obj, filename):
     elif os.path.exists(path3):
         filename = path3 + filename
     else:
-        print "Please insert the path to the folder the where object will be saved."
-    
+        print "Please insert the path to the folder the where object will be "
+        "saved."
+
     with open(filename, 'wb') as output:
         pickle.dump(obj, output)
 
 
 def load_object(filename):
-  
-    path1 = "/Users/joseesteves/Documents/Erasmus/DTU/Data Mining/Git/Repository/turbot/learn/"
+
+    path1 = "/Users/joseesteves/Documents/Erasmus/DTU/Data Mining/Git/"
+    "Repository/turbot/learn/"
     path2 = "/home/beljul/DTU/Data mining using Python/Project/turbot/learn/"
-    path3 = "/home/jens/Documents/DTU/Data Mining Using Python/Project/turbot/learn/"
+    path3 = "/home/jens/Documents/DTU/Data Mining Using Python/Project/turbot/"
+    "learn/"
 
     if os.path.exists(path1):
         path = path1 + filename
@@ -40,14 +45,15 @@ def load_object(filename):
     elif os.path.exists(path3):
         path = path3 + filename
     else:
-        print "Please insert the path to the folder the where object will be loaded from."
-    
+        print "Please insert the path to the folder the where "
+        "object will be loaded from."
+
     with open(path, 'rb') as input:
         return pickle.load(input)
 
 
 def update_classifiers():
-    
+
     trainTypeQClassifier = dialog.trainTypeQuestion()
     trainWhQuestionClassifier = dialog.trainWhQuestion(1)
     trainDescOtherQuestionClassifier = dialog.trainWhQuestion(2)
@@ -69,4 +75,4 @@ def update_classifiers():
     save_object(trainSentencesMarkov.getMarkov(), 'markovSentences.pkl')
 
 # Call function update_classifiers to update the classifier files
-#update_classifiers()
+update_classifiers()
